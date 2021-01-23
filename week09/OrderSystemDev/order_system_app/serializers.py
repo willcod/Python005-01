d7c599f055cbfdeb92fbbbeda61e8bf0766c8ab7
willcod <william.wow@icloud.com>
2021-01-23 21:29:15 +0800

    Week09 Homework

--- week09/OrderSystemDev/order_system_app/serializers.py
+++ week09/OrderSystemDev/order_system_app/serializers.py
@@ -1,20 +1,10 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Order
 
 
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('id', 'product', 'number')
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        field = ['url', 'name']
 
